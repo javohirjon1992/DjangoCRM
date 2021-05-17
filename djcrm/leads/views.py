cd565b0d9751a193599b6235fbd5c531a9c92fce
javohirjon1992 <javohirjon.1992@gmail.com>
2021-05-17 12:27:02 +0500

    Initial commit

--- djcrm/leads/views.py
+++ djcrm/leads/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def home_page(request):
+    context = {
+        "name":"Jonata",
+        "age": 23
+    }
+    return render(request, 'second.html', context)
