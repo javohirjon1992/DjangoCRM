d4c4a03147633a14f2a0b181b327d2888e0a1d99
javohirjon1992 <javohirjon.1992@gmail.com>
2021-05-18 17:25:05 +0500

    Initial commited

--- djcrm/leads/views.py
+++ djcrm/leads/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
+from .forms import LeadForm
 
 
 
-def home_page(request):
+def lead_list(request):
+    leads = Lead.objects.all()
     context = {
-        "name":"Jonata",
-        "age": 23
+        "leads": leads,
     }
-    return render(request, 'second.html', context)
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    print(pk)
+    lead = Lead.objects.get(id=pk)
+    print(lead)
+    context = {
+        "lead": lead,
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        print("Receiving a post request")
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print("The form is valid")
+            print(form.cleaned_data)
+
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, "leads/lead_create.html", context)
